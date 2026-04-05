00b50983c4f06cf815b02505be15ab27f629d002
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-05 23:12:33 +0300

    Implement solve_cholesky to solve 1st task
    * Removed unnecessary f function.
    * Fixed create_L calculation logic.
    * Add forward and back substitution solving for F and X.
    * Results are printed together with the residual for verifying that A * X = F(X)

--- Project_2/main.py
+++ Project_2/main.py
@@ -1,8 +1,11 @@
 # 6: 6, 17
-import numpy as np
 
-def f(x, c):
-   return c * (x**2 + 1)
+# A X = F
+# L L^T = A
+# L L^T X = F
+# L^T X = Y
+# L Y = F
+import numpy as np
 
 def create_A(N):
    A = np.zeros((N, N), dtype=float)
@@ -13,26 +16,34 @@ def create_A(N):
 
    return A
 
-def create_F(N, X):
+def create_F(X, N):
    c = 1 / (N + 1)**2
    return c * (X**2 + 1)
 
-def create_L(A):
-   N = len(A[0])
+def create_L(A, N):
    L = np.zeros((N, N))
-   L[k][k] = np.sqrt(A[k][k])
+   L[0][0] = np.sqrt(A[0][0])
    
    for k in range(N):
       for j in range(k):
-         L[k][j] = (A[j][k] - np.sum(L[j] * L[k])) / L[j][j]
+         L[k][j] = (A[j][k] - np.sum(L[j][:j] * L[k][:j])) / L[j][j]
 
-      L[k][k] = np.sqrt(A[k][k] - np.sum(L[k]**2)) # Find diagonal l
+      L[k][k] = np.sqrt(A[k][k] - np.sum(L[k][:k]**2)) # Find diagonal l
 
    return L
 
-def solve_cholesky(A, F):
-   L = create_L(A)
-   return 1
+def solve_cholesky(A, F, N):
+   L = create_L(A, N)
+   Y = np.zeros(N)
+   X = np.zeros(N)
+
+   for i in range(N): # L Y = F
+      Y[i] = (F[i] - np.sum(L[i][:i] * Y[:i])) / L[i][i]
+
+   for i in range(N-1, -1, -1): # L^T X = Y
+      X[i] = (Y[i] - np.sum(L.T[i][i+1:] * X[i+1:])) / L[i][i]
+
+   return X
 
 def solve_1(N):
    epsilon = 0.01 # Precision
@@ -40,9 +51,9 @@ def solve_1(N):
    X = np.zeros(N)
 
    while True:
-      F = create_F(N, X)
+      F = create_F(X, N)
 
-      X_new = solve_cholesky(A, F)
+      X_new = solve_cholesky(A, F, N)
 
       if np.max(np.abs(X_new - X)) < epsilon:
          break
@@ -52,9 +63,13 @@ def solve_1(N):
    return X
 
 def main():
-   N = 4 # Matrix size
+   N = 4 # Matrix size N x N
+   A = create_A(N)
 
-   solve_1(N)
+   result = solve_1(N)
+
+   print("Residual:", np.max(np.abs(A @ result - create_F(result, N))))
+   print(result)
 
    return 1
 
