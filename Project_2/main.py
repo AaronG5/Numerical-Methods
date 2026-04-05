7bea3209d1c165276497790c868331dabbac89b4
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-05 22:03:30 +0300

    Implement helper functions for solving using cholesky decomposition
    Implemented functions:
    * create_A - creates A matrix
    * create_F - creates equatio vector
    * create_L - creates a lower triangle shaped matrix using cholesky decomposition
    * solve_1 - solves the first part of the project

--- Project_2/main.py
+++ Project_2/main.py
@@ -1,21 +1,62 @@
 # 6: 6, 17
 import numpy as np
 
-def solve_1():
-   epsilon = 0.01 # Precision
+def f(x, c):
+   return c * (x**2 + 1)
 
-   return 1
-
-def main():
-   N = 4 # Matrix size
-
-   A = np.zeros((N, N), dtype=int)
+def create_A(N):
+   A = np.zeros((N, N), dtype=float)
 
    np.fill_diagonal(A, 2)
    np.fill_diagonal(A[1:], -1)
    np.fill_diagonal(A[:, 1:], -1)
 
-   return
+   return A
+
+def create_F(N, X):
+   c = 1 / (N + 1)**2
+   return c * (X**2 + 1)
+
+def create_L(A):
+   N = len(A[0])
+   L = np.zeros((N, N))
+   L[k][k] = np.sqrt(A[k][k])
+   
+   for k in range(N):
+      for j in range(k):
+         L[k][j] = (A[j][k] - np.sum(L[j] * L[k])) / L[j][j]
+
+      L[k][k] = np.sqrt(A[k][k] - np.sum(L[k]**2)) # Find diagonal l
+
+   return L
+
+def solve_cholesky(A, F):
+   L = create_L(A)
+   return 1
+
+def solve_1(N):
+   epsilon = 0.01 # Precision
+   A = create_A(N)
+   X = np.zeros(N)
+
+   while True:
+      F = create_F(N, X)
+
+      X_new = solve_cholesky(A, F)
+
+      if np.max(np.abs(X_new - X)) < epsilon:
+         break
+
+      X = X_new
+
+   return X
+
+def main():
+   N = 4 # Matrix size
+
+   solve_1(N)
+
+   return 1
 
 
 if __name__ == "__main__":
