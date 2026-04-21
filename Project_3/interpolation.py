c451b8d4f2e162d5efabb7f95e5058231bf54e12
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-21 23:34:34 +0300

    Define function to create points for interpolation

--- Project_3/interpolation.py
+++ Project_3/interpolation.py
@@ -1,4 +1,12 @@
 # 6 (1, 5)
+import numpy as np
+
 
 def func(x):
    return (1 + x**2) / (7 + x**3)
+
+def get_points(function, a, b): # Interval [a; b]
+   x = np.linspace(a, b, 11)
+   y = function(x)
+   return x, y
+
