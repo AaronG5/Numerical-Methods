2ad84ee5e43ab4312097ff76d061433fdbaa7c9e
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-22 00:10:56 +0300

    Create linear_interpolation class wit coressponding methods
     * Implemented slope coefficient calculation upon object initialization.
     * Implement evaluation of any point in interpolation interval using linear interpolation.

--- Project_3/interpolation.py
+++ Project_3/interpolation.py
@@ -10,3 +10,30 @@ def get_points(function, a, b): # Interval [a; b]
    y = function(x)
    return x, y
 
+class linear_interpolation():
+   def __init__(self, x, y):
+      self.x = x
+      self.y = y
+      self.m = self.create_slope_coefficients(x, y) # Tiesių nuolydžio koeficientai
+
+   def create_slope_coefficients(self, x, y):
+      # m = []
+      # for i in range(10):
+      #    m.append((y[i+1] - y[i]) / (x[i+1] - x[i]))
+      # return m
+
+      return np.diff(y) / np.diff(x) # Vectorization baby!
+   
+   def evaluate(self, x):
+      if x < self.x[0] or x > self.x[-1]:
+         raise ValueError(f"x={x} is outside the interpolation interval")
+      
+      for i in range(10):
+         if self.x[i] <= x <= self.x[i+1]:
+            return self.y[i] + self.m[i] * (x - self.x[i])
+
+def main():
+   return 0
+
+if __name__ == "__main__":
+   main()
