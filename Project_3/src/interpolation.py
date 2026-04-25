6239f97150d7748de7120bb4fa7ca305c326bb09
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-04-26 01:49:25 +0300

    Implement built-in python functions for interpolation
     * Implemented "interp1d", "CubicSpline" and "lagrange" interpolation methods from scipy.interpolate.
     * Finished find_value method for QuadraticSpline class.
     * Renamed solve to find_coefficience for more concise naming.

--- Project_3/src/interpolation.py
+++ Project_3/src/interpolation.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+from scipy.interpolate import interp1d, CubicSpline, lagrange
 import os
 
 def func(x):
@@ -16,7 +17,7 @@ def div_diff(x, y): # Divided difference of n-th row
    
    return (div_diff(x[1:], y[1:]) - div_diff(x[:-1], y[:-1]))/ (x[-1] - x[0])
 
-def get_points(function, a, b): # Interval [a; b]
+def get_points(function, a, b): # Intervalas [a; b]
    x = np.linspace(a, b, 11)
    y = function(x)
    return x, y
@@ -31,8 +32,6 @@ class LinearInterpolation():
       return np.diff(y) / np.diff(x)
    
    def evaluate(self, x):
-      L = None
-      
       if x < self.x[0] or x > self.x[-1]:
          raise ValueError(f"x={x} is outside the interpolation interval")
       
@@ -61,7 +60,7 @@ class LinearInterpolation():
             label='Dalimis tiesinis interpoliavimas' if i == 0 else '_nolegend_'
          )
 
-      plt.plot(self.x, self.y, 'o', color='red', label='Interpoliavimo mazgai')
+      plt.plot(self.x, self.y, 'o', color='red', label='Mazgai')
       
       interval = np.linspace(self.x[0], self.x[-1], 100)
       plt.plot(interval, func(interval), color='orange', alpha=0.8, label='Duota funkcija')
@@ -77,7 +76,7 @@ class QuadraticSpline():
       self.y = y
       self.coef = []
 
-   def solve(self, e):
+   def find_coefficients(self, e):
       e = np.tan(np.radians(e))
       self.coef = []
       for i in range(10):
@@ -87,14 +86,17 @@ class QuadraticSpline():
                         (2 * self.x[j], 1, 0)))
          B = np.array((self.y[i], self.y[i+1], e))
          solution = np.linalg.solve(A, B)
-         print(solution)
          self.coef.append(solution) # Choleskio ir greičiausio nuolydžio metodai čia netinka
          e = 2 * self.coef[i][0] * self.x[i+1] + self.coef[i][1]
 
    def find_value(self, x):
       if x < self.x[0] or x > self.x[-1]:
          raise ValueError(f"x={x} is outside the interpolation interval")
-
+      
+      for i in range(10):
+         if self.x[i] <= x <= self.x[i+1]:
+            return self.coef[i][0] * x**2 + self.coef[i][1] * x + self.coef[i][2]
+         
    def _get_spline_points(self):
       x_all, y_all = [], []
       for i in range(10):
@@ -112,7 +114,7 @@ class QuadraticSpline():
 
       x_sp, y_sp = self._get_spline_points()
       plt.plot(x_sp, y_sp, linestyle='solid', color='blue', label='Kvadratinis splainas')
-      plt.plot(self.x, self.y, 'o', color='red', label='Interpoliavimo mazgai')
+      plt.plot(self.x, self.y, 'o', color='red', label='Mazgai')
       
       interval = np.linspace(self.x[0], self.x[-1], 100)
       plt.plot(interval, func(interval), color='orange', alpha=0.8, label='Duota funkcija')
@@ -135,7 +137,7 @@ class QuadraticSpline():
       ax.legend()
 
       def update(alpha):
-         self.solve(alpha)
+         self.find_coefficients(alpha)
          x_sp, y_sp = self._get_spline_points()
          spline_line.set_data(x_sp, y_sp)
          alpha_text.set_text(f'α = {alpha}°')
@@ -145,6 +147,30 @@ class QuadraticSpline():
       ani.save(os.path.join(result_dir, 'spline_animation.gif'), writer='pillow', fps=15)
       plt.show()
 
+def interpolate_built_in(x, y, result_dir):
+   x_plot = np.linspace(x[0], x[-1], 100)
+   i = 1
+   for method in (interp1d, CubicSpline, lagrange): # lagrange sukuria vieną (n-1-ojo laipsnio) polinomą
+      plt.figure(figsize=(6, 4))
+      plt.title(f'Python funckijos: {i} {method.__name__}')
+      plt.xlim((x[0] - 0.1, x[-1] + 0.1))
+      plt.ylim((min(y) - 0.01, max(y) + 0.01)) 
+
+      if method == interp1d:
+         m = method(x, y, kind='quadratic')
+      else:
+         m = method(x, y)
+
+      plt.plot(x_plot, m(x_plot), label=method.__name__)
+      plt.plot(x_plot, func(x_plot), color='orange', alpha=0.8, label='Duota funkcija')
+      plt.plot(x, y, 'o', color='red', label='Mazgai')
+      plt.legend()
+
+      filename = f'{i}_{method.__name__}.png'
+      graph_filepath = os.path.join(result_dir, filename)
+      plt.savefig(graph_filepath, dpi=300)
+      i += 1
+
 def main():
    result_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'result')
    os.makedirs(result_dir, exist_ok=True)
@@ -153,21 +179,25 @@ def main():
    b = 4
    x, y = get_points(func, a, b)
 
-   # linear_interpolation = LinearInterpolation(x, y)
-   # for point in (2.8, 1):
-   #    result, est_error, real_error = linear_interpolation.evaluate(point)
-   #    print(f'Test point:      {point}\n'
-   #          f'Result:          {result:.5f}\n' \
-   #          f'Estimated error: {est_error:.5f}\n' \
-   #          f'Actual error:    {real_error:.5f}\n' \
-   #          f'Difference:      {abs(est_error - real_error):.5f}\n')
+   linear_interpolation = LinearInterpolation(x, y)
+   for point in (2.8, 1):
+      result, est_error, real_error = linear_interpolation.evaluate(point)
+      print(f'Test point:      {point}\n'
+            f'Result:          {result:.5f}\n' \
+            f'Estimated error: {est_error:.5f}\n' \
+            f'Actual error:    {real_error:.5f}\n' \
+            f'Difference:      {abs(est_error - real_error):.5f}\n')
 
-   # linear_interpolation.plot(result_dir)
+   linear_interpolation.plot(result_dir)
 
    quad_spline = QuadraticSpline(x, y)
-   quad_spline.solve(-9)
+   quad_spline.find_coefficients(-9) # -9 laipsniai tinka labiausiai
+   print(quad_spline.find_value(2.8))
    quad_spline.plot(result_dir)
-   # quad_spline.animate(result_dir)
+
+   quad_spline.animate(result_dir)
+
+   interpolate_built_in(x, y, result_dir)
 
    return 0
 
