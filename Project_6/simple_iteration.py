58687a8605827158eacf0ddc71f5f28d3c825a08
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-06-21 22:52:41 +0300

    Add threshold for norm

--- Project_6/simple_iteration.py
+++ Project_6/simple_iteration.py
@@ -5,15 +5,18 @@ def easy_system(X):
    y = X[1]
    x_res = 0.125 * (8 * x - 4 * x**2 + y**2 + 1)
    y_res = 0.25 * (2 * x - x**2 + 4 * y - y**2 + 3)
-   return (x_res, y_res)
+   return np.array((x_res, y_res))
 
 def hard_system(X):
    x = X[0]
    y = X[1]
    z = X[2]
 
-def simple_iteration(system, X, max_iter = 1000):
+def simple_iteration(system, X, threshold = 1e-6, max_iter = 1000):
    for i in range(max_iter):
-      X = system(X)
+      X_old = X.copy()
+      X = system(X_old)
+      if np.linalg.norm(X - X_old, np.inf) < threshold:
+         break 
 
-   return X
+   return X, i
